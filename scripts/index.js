c3679c965d4f0a7258fa51cb34e275f6b3175dc0
Александр Парфенов <7081275@gmail.com>
2021-12-11 17:28:56 +0300

    feat: добавил alt к изображениям

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__container');
-const editButton = document.querySelector('.profile__info-editbutton');
-const closeButton = document.querySelector('.popup__close-icon');
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__container');
+let editButton = document.querySelector('.profile__info-editbutton');
+let closeButton = document.querySelector('.popup__close-icon');
 function actionPopup() {
   popup.classList.toggle('popup_opened');
   popupForm.classList.toggle('popup_opened-container');
@@ -14,10 +14,10 @@ function closePopup() {
 }
 editButton.addEventListener('click', actionPopup);
 closeButton.addEventListener('click', closePopup);
-const profileTitle = document.querySelector('.profile__info-title');
-const profileSubtitle = document.querySelector('.profile__info-subtitle');
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__occupation');
+let profileTitle = document.querySelector('.profile__info-title');
+let profileSubtitle = document.querySelector('.profile__info-subtitle');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__occupation');
 function formSubmitHandler (evt) {
     evt.preventDefault();
   profileTitle.textContent = nameInput.value;
