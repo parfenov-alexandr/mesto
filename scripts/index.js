03ae197c8e5595f0f692766b544b2b38299e23fb
Александр Парфенов <7081275@gmail.com>
2021-12-18 14:57:34 +0300

    Исправления после 1 проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,25 @@
 let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__container');
-let editButton = document.querySelector('.profile__info-editbutton');
+let popupForm = document.querySelector('.form')
+let editButton = document.querySelector('.profile__info-edit-button');
 let closeButton = document.querySelector('.popup__close-icon');
-function actionPopup() {
-  popup.classList.toggle('popup_opened');
-  popupForm.classList.toggle('popup_opened-container');
-}
-function closePopup() {
-  popup.classList.toggle('popup_opened');
-  popupForm.classList.toggle('popup_opened-container');
-  nameInput.value = "";
-  jobInput.value = "";
-}
-editButton.addEventListener('click', actionPopup);
-closeButton.addEventListener('click', closePopup);
 let profileTitle = document.querySelector('.profile__info-title');
 let profileSubtitle = document.querySelector('.profile__info-subtitle');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__occupation');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#occupation');
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 function formSubmitHandler (evt) {
     evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  actionPopup();
+  closePopup();
 }
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
